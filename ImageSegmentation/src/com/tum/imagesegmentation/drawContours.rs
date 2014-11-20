4bfad5803808ef12795dbe5078d5173ebe3c0093
Sebastian Soyer <soyer.sebastian@gmail.com>
2014-11-20 01:07:50 +0100

    Execution in separate thread implemented

--- ImageSegmentation/src/com/tum/imagesegmentation/drawContours.rs
+++ ImageSegmentation/src/com/tum/imagesegmentation/drawContours.rs
@@ -13,7 +13,7 @@ int contourWidth = 1;
 static int height;
 static int width;
 
-void root(const float *v_in, uchar4 *v_out, const void *usrData, uint32_t x, uint32_t y) {
+void root(const uchar4 *v_in, uchar4 *v_out, const void *usrData, uint32_t x, uint32_t y) {
 
     //float4 apixel = rsUnpackColor8888(*v_in);
     //float3 pixel = apixel.rgb;
@@ -24,12 +24,22 @@ void root(const float *v_in, uchar4 *v_out, const void *usrData, uint32_t x, uin
     color.b = 0;
     color.a = 255;
     
-    if (*v_in >= threshold && fmin(fmin(fmin(fmin(fmin(fmin(fmin(fmin(
-    		rsGetElementAt_float(u, x, y-1), rsGetElementAt_float(u, x, y)), 
-    		rsGetElementAt_float(u, x, y+1)), rsGetElementAt_float(u, x-1, y-1)), 
-    		rsGetElementAt_float(u, x+1, y-1)), rsGetElementAt_float(u, x-1, y+1)), 
-    		rsGetElementAt_float(u, x+1, y+1)), rsGetElementAt_float(u, x-1, y)), 
-    		rsGetElementAt_float(u, x+1, y)) < threshold) {
+    float4 pixel1 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x, y-1));
+    float4 pixel2 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x, y));
+    float4 pixel3 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x, y+1));
+    float4 pixel4 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x-1, y-1));
+    float4 pixel5 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x+1, y-1));
+    float4 pixel6 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x-1, y+1));
+    float4 pixel7 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x-1, y));
+    float4 pixel8 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x+1, y));
+    float4 pixel9 = rsUnpackColor8888(rsGetElementAt_uchar4(u, x+1, y+1));
+    
+    if (rsUnpackColor8888(*v_in).r >= threshold && fmin(fmin(fmin(fmin(fmin(fmin(fmin(fmin(
+    		pixel1.r, pixel2.r), 
+    		pixel3.r), pixel4.r), 
+    		pixel5.r), pixel6.r), 
+    		pixel7.r), pixel8.r), 
+    		(rsGetElementAt_uchar4(u, x+1, y)).r) < threshold) {
     	if (contourWidth > 1) {
     		for(int y_paint = 0; y_paint < contourWidth && (y - ((int)(contourWidth / 2.0)) + y_paint) < height; y_paint++) {
     			for(int x_paint = 0; x_paint < contourWidth && (x - ((int)(contourWidth / 2.0)) + x_paint) < width; x_paint++) {
